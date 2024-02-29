a9a75840d62988f0d29d6b86255fd5b78374ab65
Paul Brémond <pbremond@student.42nice.fr>
2024-02-29 23:36:48 +0100

    add calloc

--- src/calloc.c
+++ src/calloc.c
@@ -6,16 +6,23 @@
 /*   By: pbremond <pbremond@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 16:33:15 by pbremond          #+#    #+#             */
-/*   Updated: 2024/02/28 17:53:35 by pbremond         ###   ########.fr       */
+/*   Updated: 2024/02/29 23:33:45 by pbremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 #include <errno.h>
+#include <stdint.h>
 
 void	*CALLOC(size_t nb_elem, size_t size)
 {
-	(void)nb_elem, (void)size;
-	errno = ENOMEM;
-	return NULL;
+	if (SIZE_MAX / size < nb_elem)	// size_t overflow
+	{
+		errno = ENOMEM;
+		return NULL;
+	}
+	else
+	{
+		return MALLOC(nb_elem * size);
+	}
 }
